f01e4c966e685d002261102c788cc7c3f95d7ce5
Bruno M Noronha <bruno.developer@pm.me>
2020-06-04 00:40:32 -0300

    modal search results

--- js/create-point.js
+++ js/create-point.js
@@ -1,10 +1,10 @@
 function populateUFs() {
     const ufSelect = document.querySelector('select[name=uf]');
     fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-        .then( res => res.json() )
-        .then( states => {
+        .then(res => res.json())
+        .then(states => {
 
-            for( state of states) {
+            for (state of states) {
                 ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
             }
         })
@@ -19,21 +19,52 @@ function getCities(event) {
     const indexOfSelectedState = event.target.selectedIndex;
     stateInput.value = event.target.options[indexOfSelectedState].text;
 
+    citySelect.innerHTML = '<option value="">Selecione a cidade</option>';
+    citySelect.disabled = true;
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
-    citySelect.innerHTML = '';
-    fetch(url).then( res => res.json())
-        .then( cities => {
+    fetch(url).then(res => res.json())
+        .then(cities => {
 
             for (city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
             citySelect.disabled = false;
         })
 };
 
+document.querySelector('select[name=uf]').addEventListener('change', getCities);
 
-const estados = document
-                .querySelector('select[name=uf]')
-                .addEventListener('change', getCities);
+const itemsToCollect = document.querySelectorAll('.items-grid li');
+
+for (item of itemsToCollect) {
+    item.addEventListener('click', hendleSelectedItem)
+};
+
+const collectedItems = document.querySelector('input[name=items]');
+let selectedItems = [];
+
+function hendleSelectedItem(event) {
+
+    const itemLi = event.target;
+    itemLi.classList.toggle('selected');
+
+    const itemId = event.target.dataset.id;
+    const alreadySelected = selectedItems.findIndex(item => { 
+        const itemFound = item == itemId;
+        return itemFound;
+     });
+
+    if (alreadySelected != -1) {
+        const filteredItems = selectedItems.filter( item => { 
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+         });
+        selectedItems = filteredItems;
+    } else {
+        selectedItems.push(itemId);
+    }
+
+    collectedItems.value = selectedItems;
+}
